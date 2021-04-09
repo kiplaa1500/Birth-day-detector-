eda867994818a0783cc395bdb0a2bcbc892b4f4c
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-04-10 00:12:54 +0300

    Accesing elements by their id

---
+++ index.js
@@ -0,0 +1,6 @@
+function names() {
+    let cc = parseInt(document.getElementById("cent").value);
+    let yy = parseInt(document.getElementById("yea").value);
+    let mm = parseInt(document.getElementById("mon").value);
+    let dd = parseInt(document.getElementById("Day").value);
+}
