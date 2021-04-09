288e50770a3bb678234674938abab2fb350b6913
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-04-10 00:20:48 +0300

    Asigning arrays to elements

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ function names() {
     let yy = parseInt(document.getElementById("yea").value);
     let mm = parseInt(document.getElementById("mon").value);
     let dd = parseInt(document.getElementById("Day").value);
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 }
