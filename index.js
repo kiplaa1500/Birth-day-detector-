f7b48b854d37d20b33e8f795ebf8303e4493cc67
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-04-15 15:25:40 +0300

    Trying to achieve akan name

--- index.js
+++ index.js
@@ -20,20 +20,15 @@ function haveAkanName() {
 
     if (day <= 0 || day > 31) {
         alert("Povide correct day details");
-    }
-    else if (months < 1 || months > 12 || months == 2 && day > 29) {
+    } else if (months < 1 || months > 12 || months == 2 && day > 29) {
         alert("Enter correct month datails !");
-    }
-    else if (years <= 0 || years > 2021) {
+    } else if (years <= 0 || years > 2021) {
         alert("Enter correct year deatils !!");
-    }
-    else if ((females === false)&&(males === false)) {
+    } else if ((females === false) && (males === false)) {
         alert("Select at-least one gender");
-    }
-    else if (males === true) {
+    } else if (males === true) {
         alert("You were born on : " + days[weekdDays] + " and your AKAN name is : " + maleNames[weekdDays]);
-    }
-    else if (females === true) {
+    } else if (females === true) {
         alert("You were born on : " + days[weekdDays] + " and your AKAN name is : " + femaleNames[weekdDays]);
     }
 
